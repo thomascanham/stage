2b4e1bc352ccc4a16b96a7aa651cc83cb77ee6c3
Tom Canham <31967070+thomascanham@users.noreply.github.com>
2020-05-21 20:25:34 +0100

    added netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,19 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/content`,
       },
     },
     `gatsby-transformer-sharp`,
